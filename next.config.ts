e272fe512c099a875d4622caecc1b21242372b41
Pushparaj <59532017+epostman16@users.noreply.github.com>
2026-06-27 14:46:01 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
